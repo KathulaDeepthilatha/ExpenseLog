2420268424ed087b7a6d7d4f9d1ac9d586b5a10d
DeepthiLatha <kathuladeepthilatha9@gmail.com>
2025-07-26 16:51:25 +0530

    Installed tailwindcss and app is running fine.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // If you have a main HTML file in the root
+    "./src/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
