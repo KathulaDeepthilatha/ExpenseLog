d530b7edb913d5678e494614265f5617e17c553b
DeepthiLatha <kathuladeepthilatha9@gmail.com>
2025-08-02 22:33:45 +0530

    Implementing Monthly setup

--- src/components/MonthlySetUp.js
+++ src/components/MonthlySetUp.js
@@ -1,7 +1,13 @@
 const MonthlySetUp = () => {
+
+    const [recurringItems, setRecurringItems] = useState([]);
+
     return(
         <div>
             <h1 className="text-2xl font-semibold mb-2">Monthly SetUp</h1>
+            <div className="flex flex-col align-middle">
+                <button type="submit" className="text-2xl align-middle mt-2 p-2 rounded-md border"><span className="text-3xl">+ </span>Add New Recurring Item</button>
+            </div>
         </div>
     )
 }
