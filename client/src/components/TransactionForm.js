09aa8d51f61bbe3f915d2dff46a57fb01a8737d0
DeepthiLatha <kathuladeepthilatha9@gmail.com>
2025-09-02 12:12:27 +0530

    ,

--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { API_URL } from "../api";
+import { API_URL } from "./api";
 
 
 const TransactionForm = ({ buttonText, type, paymentMethod, onTransactionAdded }) => {
